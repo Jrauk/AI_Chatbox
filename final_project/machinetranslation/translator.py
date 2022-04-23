41a77bfd67342d7b89e13ecabb96cf5af4e27b20
Jrauk <103428723+Jrauk@users.noreply.github.com>
2022-04-23 22:18:50 +0200

    new naming policy to be even all over

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -31,13 +31,13 @@ language_translator.set_service_url(
 def english_to_french(english_text):
     '''translates english input text to french output'''
     translation= language_translator.translate(
-        text=english_text, model_id="en-fr").get_result()
-    french_text= translation['translations'][0]['translation']
-    return french_text
+        text= english_text, model_id="en-fr").get_result()
+    translated_text= translation['translations'][0]['translation']
+    return translated_text
 
 def french_to_english(french_text):
     '''translates french input text to english output'''
     translation= language_translator.translate(
-        text=french_text, model_id="fr-en").get_result()
-    english_text= translation['translations'][0]['translation']
-    return english_text
+        text= french_text, model_id="fr-en").get_result()
+    translated_text= translation['translations'][0]['translation']
+    return translated_text
