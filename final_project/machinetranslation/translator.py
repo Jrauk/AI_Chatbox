594e63d578e8230fcb201d171088670b9e017086
Jrauk <103428723+Jrauk@users.noreply.github.com>
2022-04-23 22:25:32 +0200

    tidying

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,31 +2,20 @@
 Imports Language translator from IBM service and codes for translation en-fr and fr-en.
 """
 import json
-
 import os
-
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
+    '''load environmental variables'''
 from dotenv import load_dotenv
-
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
 
 authenticator = IAMAuthenticator(apikey)
-language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
-)
-
+language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
 language_translator.set_disable_ssl_verification(True)
-
-language_translator.set_service_url(
-    'https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/09074670-8c5c-4dcc-8259-25dc658ac122')
-
-
+language_translator.set_service_url(url)
 
 def english_to_french(english_text):
     '''translates english input text to french output'''
