b991797554889a4d4f665a7c47118ac7b7cbcdad
Jrauk <103428723+Jrauk@users.noreply.github.com>
2022-04-23 21:30:37 +0200

    updated snake _ cases

--- final_project/static/mywebscript.js
+++ final_project/static/mywebscript.js
@@ -1,5 +1,5 @@
-let translateToFrench = ()=>{
-    textToTranslate = document.getElementById("textToTranslate").value;
+let translate_to_french = ()=>{
+    text_to_translate = document.getElementById("text_to_translate").value;
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -7,12 +7,12 @@ let translateToFrench = ()=>{
             document.getElementById("translated_text").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "englishToFrench?textToTranslate"+"="+textToTranslate, true);
+    xhttp.open("GET", "english_to_french?text_to_translate"+"="+text_to_translate, true);
     xhttp.send();
 }
 
-let translateToEnglish = ()=>{
-    textToTranslate = document.getElementById("textToTranslate").value;
+let translate_to_english = ()=>{
+    text_to_translate = document.getElementById("text_to_translate").value;
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -20,7 +20,7 @@ let translateToEnglish = ()=>{
             document.getElementById("translated_text").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "frenchToEnglish?textToTranslate"+"="+textToTranslate, true);
+    xhttp.open("GET", "french_to_english?text_to_translate"+"="+text_to_translate, true);
     xhttp.send();
 }
 
